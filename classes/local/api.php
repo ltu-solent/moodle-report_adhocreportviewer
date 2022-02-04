b2cda1461cb365c7caa5bdbb6860ce51beff3687
Mark Sharp <mark.sharp@solent.ac.uk>
2022-02-04 11:04:59 +0000

    reorg api

--- classes/local/api.php
+++ classes/local/api.php
@@ -32,35 +32,6 @@ defined('MOODLE_INTERNAL') || die();
 
 class api {
     
-    public static function canview($user, $id): bool {
-        global $DB;
-        if (is_siteadmin()) {
-            return true;
-        }
-        $recordexists = $DB->record_exists('report_adhocreportviewer', ['cqid' => $id, 'accesstype' => 'user', 'accessid' => $user->id]);
-        return $recordexists;
-    }
-
-    public static function canmanage(): bool {
-        global $DB;
-        if (is_siteadmin()) {
-            return true;
-        }
-        return has_capability('report/adhocreportviewer:manage', context_system::instance());
-    }
-
-    public static function viewablereports($user): array {
-        global $DB;
-        if (is_siteadmin()) {
-           return $DB->get_records('report_customsql_queries');
-        }
-        $viewables = $DB->get_records('report_adhocreportviewer', ['accesstype' => 'user' , 'accessid' => $user->id], '', 'cqid');
-        $ids = array_keys($viewables);
-        list($insql, $inparams) = $DB->get_in_or_equal($ids);
-        $reports = $DB->get_records_sql("SELECT * FROM {report_customsql_queries} WHERE id $insql", $inparams);
-        return $reports;
-    }
-
     public static function add_access($cqid, $accessid, $accesstype = 'user') {
         global $DB;
         $record = new stdClass();
@@ -70,6 +41,56 @@ class api {
         $DB->insert_record('report_adhocreportviewer', $record);
     }
 
+    public static function canmanageaccess(): bool {
+        global $DB;
+        if (is_siteadmin()) {
+            return true;
+        }
+        return has_capability('report/adhocreportviewer:manage', context_system::instance());
+    }
+
+    public static function canview($user, $id): bool {
+        global $DB;
+        if (is_siteadmin()) {
+            return true;
+        }
+        $recordexists = $DB->record_exists('report_adhocreportviewer', ['cqid' => $id, 'accesstype' => 'user', 'accessid' => $user->id]);
+        return $recordexists;
+    }
+
+    /**
+     * Returns list of categories for given reports and puts reports in those categories grouped by run method.
+     *
+     * @param array $reports
+     * @return array
+     */
+    public static function categories($reports): array {
+        global $DB;
+        if (count($reports) == 0) {
+            return [];
+        }
+        $cats = [];
+        foreach ($reports as $report) {
+            $cat = $report->categoryid;
+            if (!in_array($cat, $cats)) {
+                $cats[] = $cat;
+            }
+        }
+        list($insql, $inparams) = $DB->get_in_or_equal($cats);
+        $categories = $DB->get_records_select('report_customsql_categories', "id {$insql}", $inparams);
+        foreach ($categories as $category) {
+            $category->types = [];
+            $category->types['manual'] = [];
+            $category->types['daily'] = [];
+            $category->types['weekly'] = [];
+            $category->types['monthly'] = [];
+        }
+        foreach ($reports as $report) {
+                $categories[$report->categoryid]->types[$report->runable][] = $report;
+        }
+        return $categories;
+    }
+
     public static function remove_access($cqid, $accessid, $accesstype = 'user') {
         global $DB;
         $DB->delete_records('report_adhocreportviewer', [
@@ -77,6 +98,33 @@ class api {
             'accessid' => $accessid,
             'accesstype' => $accesstype
         ]);
+    }
+
+    public static function viewablereports($user): array {
+        global $DB;
+        if (is_siteadmin()) {
+           return $DB->get_records('report_customsql_queries');
+        }
+        $viewables = $DB->get_records('report_adhocreportviewer', ['accesstype' => 'user' , 'accessid' => $user->id], '', 'cqid');
+        if (count($viewables) == 0) {
+            return [];
+        }
+        $ids = array_keys($viewables);
+        list($insql, $inparams) = $DB->get_in_or_equal($ids);
+        $reports = $DB->get_records_sql("SELECT * FROM {report_customsql_queries} WHERE id $insql", $inparams);
+        return $reports;
+    }
+
+    public static function sortcats($records): array {
+        $sortedrecords = [];
+
+        foreach ($records as $record) {
+            $sortedrecords[$record->name] = $record;
+        }
+
+        ksort($sortedrecords, SORT_NATURAL);
+
+        return $sortedrecords;
     }
 }
 
